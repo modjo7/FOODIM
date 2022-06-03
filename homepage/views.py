6c0fb1a7f3012bd7d885e3508b35c6e4701ad8f4
modjo <modjo7@naver.com>
2022-06-03 11:15:22 +0900

    0.2 with baseline 0603 : Notice draft feature is implemented.

--- homepage/views.py
+++ homepage/views.py
@@ -12,16 +12,24 @@ class HomeView(View):
     template_name = "home.html"
     def get(self, request):        
         labels = []
-        data = []        
+        data = []
+        timeleft = []
+        timeleft_user = []
         stockqueryset = Stock.objects.filter(is_deleted=False).order_by('-quantity')
         for item in stockqueryset:
             labels.append(item.name)
             data.append(item.quantity)
+            if (item.timeleft <= 0): # if timeleft 0,1,2, the notice will be added.
+                timeleft.append(item.name + " : " + str(item.timeleft))
+                timeleft_user.append(item.username)
         sales = SaleArticle.objects.order_by('-time')[:3]
         purchases = PurchaseArticle.objects.order_by('-time')[:3]
+
         context = {
             'labels'    : labels,
             'data'      : data,
+            'timeleft'  : timeleft,
+            'timeleft_user': timeleft_user,
             'sales'     : sales,
             'purchases' : purchases
         }
